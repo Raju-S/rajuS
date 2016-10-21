5fc79aaca9b5614ecae9ebcfc1f2583a405bd13c
Raju-S <rasuraju80@gmail.com>
2016-10-21 12:46:02 +0530

    Add files via upload

---
+++ pos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("\n Enter the number:");
+scanf("%d",&n);
+if(n>0)
+ printf("\n POsitive");
+else
+ printf("\n Negative");
+getch();
+}
