0d3604cd815743dc56a9e6ea6a8a21a503456a33
Raju-S <rasuraju80@gmail.com>
2016-12-21 11:46:27 +0530

    Add files via upload

---
+++ posi.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+int n;
+printf("\n Enter the number:");
+scanf("%d",&n);
+if(n>=0)
+{
+printf("\n The no is positive.");
+}
+else
+{
+printf("\n The no is negative.");
+}
+}
